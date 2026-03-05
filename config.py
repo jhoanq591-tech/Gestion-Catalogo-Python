fc89881b279e55b5785116889fbb458a94913cb8
jhoanq591 <jhoanq591@gmail.com>
2026-03-05 15:15:43 -0500

    feat(config): Agregamos las configuracioes visuales.

---
+++ config.py
@@ -0,0 +1,29 @@
+ARCHIVO_JSON = "empanadas.json"
+
+ANCHO_UI = 62
+
+
+class Color:
+
+    RESET    = "\033[0m"
+    BOLD     = "\033[1m"
+    DIM      = "\033[2m"
+
+    NEGRO    = "\033[30m"
+    ROJO     = "\033[31m"
+    VERDE    = "\033[32m"
+    AMARILLO = "\033[33m"
+    AZUL     = "\033[34m"
+    MAGENTA  = "\033[35m"
+    CYAN     = "\033[36m"
+    BLANCO   = "\033[37m"
+
+    BG_NEGRO    = "\033[40m"
+    BG_ROJO     = "\033[41m"
+    BG_VERDE    = "\033[42m"
+    BG_AMARILLO = "\033[43m"
+    BG_AZUL     = "\033[44m"
+    BG_MARRON   = "\033[45m"
+    BG_CYAN     = "\033[46m"
+
+C = Color()
